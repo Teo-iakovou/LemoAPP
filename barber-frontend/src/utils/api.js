bb497f2ad9f7ed380776775d578109d57b966c42
Teo-iakovou <thedoros09@gmail.com>
2024-12-24 16:42:45 +0200

    api.js

--- barber-frontend/src/utils/api.js
+++ barber-frontend/src/utils/api.js
@@ -1,5 +1,4 @@
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api";
+const API_BASE_URL = "https://lemoapp-production.up.railway.app/api";
 
 export const createAppointment = async (appointmentData) => {
   const response = await fetch(`${API_BASE_URL}/appointments`, {
