eac41a67c505fee6ed9b49b3754e2a23f4c5a51e
Teo-iakovou <thedoros09@gmail.com>
2025-01-18 16:58:08 +0200

    navbar

--- barber-frontend/src/_components/Navbar.jsx
+++ barber-frontend/src/_components/Navbar.jsx
@@ -6,21 +6,42 @@ import {
   FaCalendarAlt,
   FaUsers,
   FaClipboard,
+  FaBars,
+  FaTimes,
 } from "react-icons/fa"; // Import icons
 import LemoLogo from "../assets/LemoLogo.png";
-import LemoBlackLogo from "../assets/LemoLogo.JPG";
 
 const Navbar = ({ isAuth, onLogout }) => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const dropdownRef = useRef(null);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false); // For sidebar toggle
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // Dropdown inside logo
+  const sidebarRef = useRef(null); // Ref for sidebar
+  const dropdownRef = useRef(null); // Ref for dropdown
   const navigate = useNavigate();
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
   const handleClickOutside = (event) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+    // Close sidebar if clicking outside
+    if (
+      sidebarRef.current &&
+      !sidebarRef.current.contains(event.target) &&
+      !event.target.closest(".hamburger-button")
+    ) {
+      setIsSidebarOpen(false);
+    }
+
+    // Close dropdown if clicking outside
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target) &&
+      !event.target.closest(".logo-button")
+    ) {
       setIsDropdownOpen(false);
     }
   };
@@ -36,103 +57,127 @@ const Navbar = ({ isAuth, onLogout }) => {
     const confirmLogout = window.confirm("Are you sure you want to log out?");
     if (confirmLogout) {
       localStorage.removeItem("token"); // Clear token
-      setIsDropdownOpen(false); // Close the dropdown
+      setIsSidebarOpen(false); // Close sidebar
       navigate("/login"); // Redirect to login page
       onLogout(); // Notify parent
     }
   };
 
   return (
-    <nav className="fixed top-0 left-0 w-full z-10 bg-purple-950 flex justify-between items-center px-6 py-[-2] shadow-md">
-      {" "}
-      {/* Left Section: Logo */}
-      <div className="flex items-center">
-        <Link to={isAuth ? "/calendar" : "/"}>
-          <img
-            src={LemoLogo}
-            alt="Lemo Barber Shop Logo"
-            className="w-16 h-16 mx-5 object-cover"
-          />
-        </Link>
-        {isAuth && (
-          <ul className="flex items-center space-x-6 hover:text-blue-500 transition">
-            {/* Home Icon */}
-            <li>
-              <Link to="/" className="hover:text-blue-500 transition">
-                <FaHome className="text-xl text-white hover:text-blue-500 transition" />
-              </Link>
-            </li>
-            {/* Calendar Icon */}
-            <li>
-              <Link to="/calendar">
-                <FaCalendarAlt className="text-xl text-white hover:text-blue-500 transition " />
-              </Link>
-            </li>
-            {/* Customers Icon */}
-            <li>
-              <Link to="/customers" className="hover:text-blue-500 transition">
-                <FaUsers className="text-xl text-white hover:text-blue-500 transition" />
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/CustomerCounts"
-                className="text-white hover:text-blue-500 transition"
-              >
-                <FaChartBar className="text-xl text-white hover:text-blue-500 transition" />
-              </Link>
-            </li>
-            <li>
-              <Link to="/NotePage">
-                <FaClipboard className="text-white hover:text-blue-500 transition"></FaClipboard>
-              </Link>
-            </li>
-          </ul>
-        )}
-      </div>
-      {/* Right Section: Profile Avatar */}
-      <div className="flex items-center space-x-6">
-        {isAuth ? (
-          <div className="relative" ref={dropdownRef}>
+    <>
+      {/* Navbar */}
+      <nav className="fixed top-0 left-0 w-full z-10 bg-purple-950 flex justify-between items-center px-4 py-2 shadow-md">
+        {/* Hamburger Menu */}
+        <button
+          className="hamburger-button text-white text-2xl"
+          onClick={toggleSidebar}
+        >
+          {isSidebarOpen ? <FaTimes /> : <FaBars />}
+        </button>
+
+        {/* Logo with Dropdown */}
+        <div className="relative" ref={dropdownRef}>
+          <button
+            className="logo-button flex items-center"
+            onClick={toggleDropdown}
+          >
             <img
-              src={LemoBlackLogo}
-              alt="LemoLogo"
-              className="w-10 h-10 rounded-full mx-auto cursor-pointer object-cover"
-              onClick={toggleDropdown}
+              src={LemoLogo}
+              alt="Lemo Barber Shop Logo"
+              className="w-12 h-12 object-cover"
             />
-            {isDropdownOpen && (
-              <div className="absolute right-0 mt-1 bg-white shadow-lg rounded-3xl py-4 px-4 w-48 z-50 border border-gray-200">
-                {/* Profile Link */}
-                <Link
-                  to="/profile"
-                  className="block px-4 py-2 text-gray-700 hover:bg-gray-100 rounded-3xl"
-                  onClick={() => setIsDropdownOpen(false)} // Close dropdown on click
-                >
-                  ΠΡΟΦΙΛ
-                </Link>
-                {/* Update Password Link */}
-                <Link
-                  to="/update-password"
-                  className="block px-4 py-2 text-gray-700 hover:bg-gray-100 rounded-3xl"
-                  onClick={() => setIsDropdownOpen(false)} // Close dropdown on click
-                >
-                  ΑΛΛΑΓΗ ΚΩΔΙΚΟΥ
-                </Link>
-                {/* Logout Button */}
-                <button
-                  onClick={handleLogout}
-                  className="w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100 rounded-3xl"
-                >
-                  ΕΞΟΔΟΣ
-                </button>
-              </div>
-            )}
-          </div>
-        ) : (
-          <div className="flex gap-4"></div>
-        )}
+          </button>
+          {isDropdownOpen && (
+            <div className="absolute top-14 right-0 bg-white shadow-lg rounded-3xl py-4 px-4 w-48 z-50 border border-gray-200">
+              {/* Profile Link */}
+              <Link
+                to="/profile"
+                className="block px-4 py-2 text-gray-700 hover:bg-gray-100 rounded-3xl"
+                onClick={() => setIsDropdownOpen(false)} // Close dropdown on click
+              >
+                ΠΡΟΦΙΛ
+              </Link>
+              {/* Update Password Link */}
+              <Link
+                to="/update-password"
+                className="block px-4 py-2 text-gray-700 hover:bg-gray-100 rounded-3xl"
+                onClick={() => setIsDropdownOpen(false)} // Close dropdown on click
+              >
+                ΑΛΛΑΓΗ ΚΩΔΙΚΟΥ
+              </Link>
+              {/* Logout Button */}
+              <button
+                onClick={handleLogout}
+                className="w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100 rounded-3xl"
+              >
+                ΕΞΟΔΟΣ
+              </button>
+            </div>
+          )}
+        </div>
+      </nav>
+
+      {/* Sidebar */}
+      <div
+        ref={sidebarRef}
+        className={`fixed top-0 left-0 h-full bg-purple-900 w-40 rounded-xl transform ${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 z-50`}
+      >
+        <ul className="flex flex-col p-4 space-y-6 text-white ">
+          <li>
+            <Link
+              to="/"
+              onClick={() => setIsSidebarOpen(false)}
+              className=" hover:text-blue-700"
+            >
+              <FaHome className="inline-block mr-2  hover:text-blue-700" />{" "}
+              Αρχική
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/calendar"
+              onClick={() => setIsSidebarOpen(false)}
+              className=" hover:text-blue-700"
+            >
+              <FaCalendarAlt className="inline-block mr-2 hover:text-blue-700" />{" "}
+              Ημερολόγιο
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/customers"
+              onClick={() => setIsSidebarOpen(false)}
+              className=" hover:text-blue-700"
+            >
+              <FaUsers className="inline-block mr-2 hover:text-blue-700" />{" "}
+              Πελάτες
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/CustomerCounts"
+              onClick={() => setIsSidebarOpen(false)}
+              className=" hover:text-blue-700"
+            >
+              <FaChartBar className="inline-block mr-2 hover:text-blue-700" />{" "}
+              Στατιστικά
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/NotePage"
+              onClick={() => setIsSidebarOpen(false)}
+              className=" hover:text-blue-700"
+            >
+              <FaClipboard className="inline-block mr-2 hover:text-blue-700" />{" "}
+              Σημειώσεις
+            </Link>
+          </li>
+        </ul>
       </div>
-    </nav>
+    </>
   );
 };
 
